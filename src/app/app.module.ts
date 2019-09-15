cac1d35e13f5e07cd1e94c9efff1b785e1aa07f9
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-09-15 12:23:06 +0100

    Landing-page complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/Pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/Pages/register/register.component';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    RegisterComponent,
+    AuthFormComponent
   ],
   imports: [
     BrowserModule,
