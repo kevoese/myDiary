b880845bdd1823e951601b2b050fe0d5f1c76a7b
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-09-19 03:27:45 +0100

    Revert "Set up login and register page"
    This reverts commit c606ae3c42924bcba9e99364bd198c9218e3956b.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { RegisterComponent } from './components/Pages/register/register.component';
+import { HomeComponent } from './components/Pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { HomeComponent } from './components/Pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/Pages/register/register.component';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    RegisterComponent,
+    AuthFormComponent
+>>>>>>> parent of c606ae3... Set up login and register page
   ],
   imports: [
     BrowserModule,
